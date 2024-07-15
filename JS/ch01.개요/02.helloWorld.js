f01241df25af305abea8fa7a5200439198d9e723
TaeKyun Shin <stk131@naver.com>
2024-07-15 12:36:30 +0900

    js

---
+++ JS/ch01.개요/02.helloWorld.js
@@ -0,0 +1,10 @@
+//자바 스크립트의 주석(comnt)
+/*반복문-여러줄에 걸쳐서 주석을 작성하는 경우*/
+console.log("Hello World!!!");
+let sum = 0;
+for (let i = 1; i <= 1000; i++) {
+  sum += i;
+}
+console.log("1에서 1000까지의 합은", sum);
+
+// 편하게 주석다는 방법-마우스로 선택한 후 Ctrl+/
