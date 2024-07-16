02133d52e96a2672a40d24dbba7fb797b847dc9b
TaeKyun Shin <stk131@naver.com>
2024-07-16 17:07:40 +0900

    js

--- JS/ch03.조건문/Q3.주사위3개.js
+++ JS/ch03.조건문/Q3.주사위3개.js
@@ -3,21 +3,46 @@
 let dice1 = Math.ceil(Math.random() * 6);
 dice2 = Math.ceil(Math.random() * 6);
 dice3 = Math.ceil(Math.random() * 6);
-price = 0;
+prize = 0;
 if (dice1 == dice2 && dice1 == dice3) {
-  price = dice1 * 1000 + 10000;
+  prize = dice1 * 1000 + 10000;
 } else if (dice1 == dice2 || dice1 == dice3) {
-  price = dice1 * 100 + 1000;
+  prize = dice1 * 100 + 1000;
 } else if (dice2 == dice3) {
-  price = dice2 * 100 + 1000;
+  prize = dice2 * 100 + 1000;
 } else {
   if (dice1 > dice2 && dice1 > dice3) {
-    price = dice1 * 100;
+    prize = dice1 * 100;
   } else if (dice2 > dice1 && dice2 > dice3) {
-    price = dice2 * 100;
+    prize = dice2 * 100;
   } else {
-    price = dice3 * 100;
+    prize = dice3 * 100;
   }
 }
 console.log(`(${dice1},${dice2},${dice3})`);
-console.log(`상금은 ${price}원 입니다.`);
+console.log(`상금은 ${prize}원 입니다.`);
+
+//Refactoring
+
+if (dice1 == dice2 && dice1 == dice3) {
+  prize = dice1 * 1000 + 10000;
+} else if (dice1 == dice2 || dice1 == dice3) {
+  prize = dice1 * 100 + 1000;
+} else if (dice2 == dice3) {
+  prize = dice2 * 100 + 1000;
+} else {
+  let maxDice = dice1 > dice2 ? dice1 : dice2;
+  maxDice = maxDice > dice3 ? maxDice : dice3;
+  prize = 100 * maxDice;
+}
+
+if (dice1 == dice2 && dice1 == dice3) {
+  prize = dice1 * 1000 + 10000;
+} else if (dice1 == dice2 || dice1 == dice3) {
+  prize = dice1 * 100 + 1000;
+} else if (dice2 == dice3) {
+  prize = dice2 * 100 + 1000;
+} else {
+  let maxDice = Math.max(dice1, dice2, dice3);
+  prize = 100 * maxDice;
+}
