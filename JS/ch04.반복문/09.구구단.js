7858b37aadcecaacd814c07607d17d5b99316cf5
TaeKyun Shin <stk131@naver.com>
2024-07-17 16:05:58 +0900

    js

---
+++ JS/ch04.반복문/09.구구단.js
@@ -0,0 +1,30 @@
+//구구단
+for (let i = 2; i <= 9; i++) {
+  for (let k = 1; k <= 9; k++) {
+    console.log(`${i}*${k}=${i * k}`);
+  }
+  console.log();
+}
+
+//다이아몬드 그리기
+let N = 4;
+for (let i = 0; i <= N; i++) {
+  let line = "";
+  for (let k = 0; k < N - i; k++) {
+    line += " ";
+  }
+  for (let k = 0; k < 2 * i + 1; k++) {
+    line += "*";
+  }
+  console.log(line);
+}
+for (let i = N - 1; i >= 0; i--) {
+  let line = "";
+  for (let k = 0; k < N - i; k++) {
+    line += " ";
+  }
+  for (let k = 0; k < 2 * i + 1; k++) {
+    line += "*";
+  }
+  console.log(line);
+}
